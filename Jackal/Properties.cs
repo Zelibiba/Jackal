cbb002dd2bffd4f7b31a490a172bb5b44df7b245
Дмитрий Дугин <dugin-di@yandex.ru>
2024-03-19 22:03:39 +0300

    Правка сетевых протоколов.

--- Jackal/Properties.cs
+++ Jackal/Properties.cs
@@ -15,7 +15,7 @@ namespace Jackal
         /// <summary>
         /// Версия программы.
         /// </summary>
-        public static string Version => "1.0.6";
+        public static string Version => "1.0.7";
 
         static Properties()
         {
